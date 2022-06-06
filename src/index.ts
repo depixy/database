5769d9cdb83d3b882a2c0147171023ce1505d815
Joshua Avalon <git@avalon.sh>
2022-06-06 17:04:31 +0800

    feat: add logging and error format

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import fastifyPlugin from "fastify-plugin";
 import { PrismaClient } from "@prisma/client";
+
 import type { Prisma } from "@prisma/client";
 
 export interface DepixyDatabaseOptions {
-  // PostgreSQL connection string
+  /**
+   * PostgreSQL connection string
+   */
   url?: string;
+  /**
+   * Log SQL query. Default to `false`.
+   */
+  logging?: boolean;
+
+  /**
+   * Prisma error format. Default to `minimal`.
+   * @see https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#errorformat
+   */
+  errorFormat?: Prisma.ErrorFormat;
 }
 
 const plugin = fastifyPlugin<DepixyDatabaseOptions>(
@@ -12,12 +25,23 @@ const plugin = fastifyPlugin<DepixyDatabaseOptions>(
     if (fastify.hasDecorator("db")) {
       throw new Error("@depixy/database has already registered");
     }
-    const { url } = opts;
-    const clientOpts: Prisma.PrismaClientOptions = {
+    const { url, errorFormat = "minimal", logging = false } = opts;
+    const db = new PrismaClient({
       datasources: url ? { db: { url } } : undefined,
-      errorFormat: "minimal"
-    };
-    const db = new PrismaClient(clientOpts);
+      errorFormat,
+      log: [
+        { emit: "event", level: "query" },
+        { emit: "event", level: "error" },
+        { emit: "event", level: "info" },
+        { emit: "event", level: "warn" }
+      ]
+    });
+    if (logging) {
+      db.$on("query", e => fastify.log.trace(e));
+      db.$on("error", e => fastify.log.error(e));
+      db.$on("info", e => fastify.log.info(e));
+      db.$on("warn", e => fastify.log.warn(e));
+    }
     fastify.decorate("db", db);
   },
   {
